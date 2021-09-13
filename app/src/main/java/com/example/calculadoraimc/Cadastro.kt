614c97ced547d6e40a06fbac070bb891f7f7079a
GabrielTSR <gabrielalvesrocha22@gmail.com>
2021-09-13 11:01:53 -0300

    pagina de cadastro

---
+++ app/src/main/java/com/example/calculadoraimc/Cadastro.kt
@@ -0,0 +1,11 @@
+package com.example.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
