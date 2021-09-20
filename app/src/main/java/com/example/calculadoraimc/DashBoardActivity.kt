d955679fe137b17ecadfe5665692e5f7614a3292
GabrielTSR <gabrielalvesrocha22@gmail.com>
2021-09-20 11:08:31 -0300

    sincronizei paletas de cores e fiz a atividade de dashboard

--- app/src/main/java/com/example/calculadoraimc/DashBoardActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/DashBoardActivity.kt
@@ -3,9 +3,9 @@ package com.example.calculadoraimc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Cadastro : AppCompatActivity() {
+class DashBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro)
+        setContentView(R.layout.activity_dash_board)
     }
 }
