fe23635d4bfaab34dee112f8b84f2b5ad918b954
GabrielTSR <gabrielalvesrocha22@gmail.com>
2021-10-04 11:20:48 -0300

    link para cadastrar

--- app/src/main/java/com/example/calculadoraimc/LoginActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.example.calculadoraimc
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,12 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         supportActionBar!!.hide()
+
+        val tvLinkCadastro = findViewById<TextView>(R.id.tv_linkParaCadastro)
+        tvLinkCadastro.setOnClickListener {
+            val abrirDatePickerActivity = Intent(this, DatePickerDialog::class.java)
+
+            startActivity(abrirDatePickerActivity)
+        }
     }
 }
